f4d8e9385510def2312442082bc92d53bfd89c1a
kirinchen <a29243685@msn.com>
2016-11-22 18:13:51 +0800

    first commit

---
+++ TestMove.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ZTesApha {
+    public class TestMove : MonoBehaviour {
+
+        // Update is called once per frame
+        void Update() {
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+            transform.Translate(new Vector3(x,y,0));
+        }
+    }
+}
