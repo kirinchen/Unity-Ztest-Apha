92451086edd66b6b690178ad20b3545dd6620b64
kirinchen <a29243685@msn.com>
2016-11-22 18:13:51 +0800

    first commit

---
+++ CameraZtestDetect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace ZTesApha {
+    public class CameraZtestDetect : MonoBehaviour {
+
+        private Camera cmr;
+        // Use this for initialization
+        void Start() {
+            cmr = GetComponent<Camera>();
+            InvokeRepeating("cast", 2.0f, 0.3f);
+        }
+
+        void cast() {
+            Vector3 fwd = cmr.transform.TransformDirection(Vector3.forward);
+
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.transform.position, fwd, out hit, 100f)) {
+                ZTesApha mr = hit.collider.GetComponent<ZTesApha>();
+                if (mr != null) {
+                    mr.detecte();
+
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ ZTesApha.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace ZTesApha {
+    public class ZTesApha : MonoBehaviour {
+
+        public float showTime = 1f;
+        public float apha = 0.075f;
+        private MeshRenderer mr;
+        private bool detected;
+        private float startAt;
+        private Material orignalMaterial;
+        public Material aphaMetial;
+
+
+        void Start() {
+            mr = GetComponent<MeshRenderer>();
+            orignalMaterial = mr.material;
+            aphaMetial.mainTexture = orignalMaterial.mainTexture;
+            Color c = orignalMaterial.color;
+            aphaMetial.color = new Color(c.r, c.g, c.b, apha);
+            aphaMetial.mainTextureOffset = orignalMaterial.mainTextureOffset;
+            aphaMetial.mainTextureScale = orignalMaterial.mainTextureScale;
+        }
+
+        public void detecte() {
+            startAt = Time.time;
+            if (!detected) {
+                StartCoroutine(cutDown());
+            }
+        }
+
+        private IEnumerator cutDown() {
+            setDetected(true);
+            while ((Time.time - startAt) <= showTime) {
+                yield return new WaitForSeconds(.1f);
+            }
+            setDetected(false);
+        }
+
+        private void setDetected(bool b) {
+            detected = b;
+            mr.material = b ? aphaMetial : orignalMaterial;
+        }
+    }
+}
